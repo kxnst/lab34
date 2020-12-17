271fc1bef47a3add9ee0879cbcd9a0bc62e2eca8
kxnst <stadnick0673595557@gmail.com>
2020-12-17 13:40:03 +0200

    lab 3.4

---
+++ lab34/Classes/ListEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab34.Classes
+{
+    public class ListEventArgs:EventArgs
+    {
+        public string Message { get; private set; }
+        public string Find { get; private set; }
+        public ListEventArgs(string e, string find)
+        {
+            Message = e;
+            Find = find;
+        }
+    }
+}
