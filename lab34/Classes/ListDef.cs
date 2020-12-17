49354d84bc101e5ccdb446fabe2f5dd2982cb698
kxnst <46329483+kxnst@users.noreply.github.com>
2020-12-17 13:44:41 +0200

    Add files via upload
    lab34

---
+++ lab34/Classes/ListDef.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace lab34.Classes
+{
+    public delegate void ListStateHandler(object sender, ListEventArgs e);
+    class ListDef
+    {
+        protected List<string> list;
+        public List<string> Val
+        {
+            get => list;
+            private set => list = value;
+        }
+        protected Type thisType;
+        protected event ListStateHandler onAdded;
+        protected event ListStateHandler onDeleted;
+        protected event ListStateHandler onClear;
+        public ListDef(string[] data)
+        {
+            thisType = GetType();
+            Val = new List<string>(data);
+        }
+
+        protected void CallEvent(ListEventArgs e, ListStateHandler handler)
+        {
+            handler?.Invoke(this,e);
+        }
+        public void Add(string find)
+        {
+            ListEventArgs e = new ListEventArgs("Added", find);
+            CallEvent(e, onAdded);
+        }
+        public void Delete(string find)
+        {
+            ListEventArgs e = new ListEventArgs("Deleted", find);
+            CallEvent(e, onDeleted);
+        }
+        public void Clear(string find)
+        {
+            ListEventArgs e = new ListEventArgs("Clear", find);
+            CallEvent(e, onClear);
+        }
+        public ListDef setField(string field,ListStateHandler handler)
+        {
+            FieldInfo info = thisType.GetField(field, BindingFlags.NonPublic | BindingFlags.Instance);
+            if(handler!=null)
+                info.SetValue(this, handler);
+            return this;
+        }
+        public void Reset()
+        {
+            Val.Clear();
+        }
+
+    }
+}
---
+++ lab34/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using lab34.Classes;
+namespace lab34
+{
+    class Program
+    {
+        public static Predicate<string> NumbersMoreThanLetters= NumLet;
+        static void Main(string[] args)
+        {
+            string[] strs = new string[] {
+            "abc","defg","klmnoprst"
+            };
+            ListDef list = new ListDef(strs);
+            list.setField("onAdded", onAdded).setField("onDeleted", onDeleted).setField("onClear", onClear);
+            NumbersMoreThanLetters = NumLet;
+            list.Delete("abc");
+        }
+        public static bool NumLet(string str)
+        {
+            Regex letters = new Regex("[a-zA-Z]");
+            Regex nums = new Regex("[0-9]");
+            return nums.Matches(str).Count > letters.Matches(str).Count;
+        }
+        public static void onAdded(object o,ListEventArgs e)
+        {
+            if((o as ListDef) != null)
+            {
+                (o as ListDef).Val.Add(e.Find);
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static void onDeleted(object o,ListEventArgs e)
+        {
+            if ((o as ListDef) != null)
+            {
+                (o as ListDef).Val.Remove(e.Find);
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static void onClear(object o, ListEventArgs e)
+        {
+            if ((o as ListDef) != null)
+            {
+                (o as ListDef).Reset();
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
